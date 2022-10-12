235bc26a917bf9007e8ae5169a9bc1d9ebbe93d0
Brooke Heck <b19heck@gmail.com>
2022-10-12 20:07:13 +0000

    add create person function

--- createPeople/index.js
+++ createPeople/index.js
@@ -17,12 +17,14 @@ const schema = dynamoose.Schema({
   },
 });
 
+const PersonModel = dynamoose.model('people-401d94', schema);
+
 module.exports = {  
 
   handler: async (event) => {
 
-    const PersonModel = dynamoose.model('people-401d94', schema);
-    let personRecord = await PersonModel.save();
+    const personData = new PersonModel({id: event.body.id, name: event.body.name, phone: event.body.phone})
+    const personRecord = await personData.save();
 
     return {
       statusCode: 200,
